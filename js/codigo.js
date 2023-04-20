c5a9cf614eb514f4ce4f36ba61df90765a2a4a09
judah <ueldjudah@gmail.com>
2023-04-20 18:58:30 -0300

    primeiro commit

---
+++ js/codigo.js
@@ -0,0 +1,19 @@
+function calcular(){
+    let peso = parseFloat(document.getElementById("peso").value);
+    let altura = parseFloat(document.getElementById("altura").value);
+    let imc = peso / (altura * altura);
+    let classificacao = "";
+
+    if (imc < 18.5){
+        classificacao = "Peso inferior ao normal";
+    } else if (imc <= 24.9){
+        classificacao = "Peso normal";
+    } else if (imc <= 29.9){
+        classificacao = "Sobrepeso";
+    } else{
+        classificacao = "Obesidade";
+    }
+
+    document.getElementById("resultado").innerHTML = "IMC: " + imc + " - " + classificacao;
+    
+}
